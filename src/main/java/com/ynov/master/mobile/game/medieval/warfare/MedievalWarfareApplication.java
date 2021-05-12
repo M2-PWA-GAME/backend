c8c86d88a6dcf846718e7b148db4424ce1ddd576
Brice Michallski <brice_michalski@carrefour.com>
2021-05-12 17:03:13 +0200

    Init maven project

---
+++ src/main/java/com/ynov/master/mobile/game/medieval/warfare/MedievalWarfareApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.master.mobile.game.medieval.warfare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedievalWarfareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedievalWarfareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynov/master/mobile/game/medieval/warfare/MedievalWarfareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.master.mobile.game.medieval.warfare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedievalWarfareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
