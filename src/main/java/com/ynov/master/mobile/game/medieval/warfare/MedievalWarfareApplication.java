3037515b683685ae30559082f8032ff3a958ef88
Brice Michallski <brice_michalski@carrefour.com>
2021-05-12 17:14:14 +0200

    Lint

--- src/main/java/com/ynov/master/mobile/game/medieval/warfare/MedievalWarfareApplication.java
+++ src/main/java/com/ynov/master/mobile/game/medieval/warfare/MedievalWarfareApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MedievalWarfareApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedievalWarfareApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MedievalWarfareApplication.class, args);
+    }
 
 }
