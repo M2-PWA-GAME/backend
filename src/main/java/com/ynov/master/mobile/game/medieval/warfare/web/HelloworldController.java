23d06ca59c972c1995055271a20de7f2f646d194
Brice Michallski <brice_michalski@carrefour.com>
2021-05-12 17:12:38 +0200

    Hello world

---
+++ src/main/java/com/ynov/master/mobile/game/medieval/warfare/web/HelloworldController.java
@@ -0,0 +1,19 @@
+package com.ynov.master.mobile.game.medieval.warfare.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloworldController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET )
+    public ResponseEntity<?> helloWorld()
+    {
+        return  ResponseEntity.status(HttpStatus.OK).body("Hello world") ;
+    }
+
+}
