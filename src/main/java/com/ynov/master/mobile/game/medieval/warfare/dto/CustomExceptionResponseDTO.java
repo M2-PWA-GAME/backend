c269c25398ed4cc80b7e51e5d8047d9d039a2b32
Brice Michallski <brice_michalski@carrefour.com>
2021-06-03 16:28:01 +0200

    Handle custom error

---
+++ src/main/java/com/ynov/master/mobile/game/medieval/warfare/dto/CustomExceptionResponseDTO.java
@@ -0,0 +1,29 @@
+package com.ynov.master.mobile.game.medieval.warfare.dto;
+
+import com.ynov.master.mobile.game.medieval.warfare.exception.CustomException;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+@Data
+public class CustomExceptionResponseDTO {
+
+    private LocalDateTime date;
+    private String error;
+    private String cause;
+    private int status;
+    private String path;
+
+    public CustomExceptionResponseDTO(CustomException e, HttpServletRequest req)
+    {
+        this.date = LocalDateTime.now();
+        this.error = e.getHttpStatus().getReasonPhrase();
+        this.cause = e.getMessage();
+        this.status = e.getHttpStatus().value();
+        this.path = req.getRequestURI();
+
+    }
+
+}
