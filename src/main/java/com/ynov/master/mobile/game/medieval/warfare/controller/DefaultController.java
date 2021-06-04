943f91bb3c4b4b6590fd3b59870005953e26f00a
Brice Michallski <brice_michalski@carrefour.com>
2021-06-04 16:54:11 +0200

    home redirection to swagger

--- src/main/java/com/ynov/master/mobile/game/medieval/warfare/controller/DefaultController.java
+++ src/main/java/com/ynov/master/mobile/game/medieval/warfare/controller/DefaultController.java
@@ -1,5 +1,7 @@
 package com.ynov.master.mobile.game.medieval.warfare.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
 
     @GetMapping("/")
-    public String greeting() {
-        return "redirect:/swagger-ui.html";
+    public ResponseEntity<Void> greeting() {
+        return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT).header("Location","/swagger-ui.html").build();
     }
 
 }
