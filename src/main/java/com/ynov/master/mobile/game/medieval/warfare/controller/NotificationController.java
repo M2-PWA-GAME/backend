8481266e51145d459e34d24e26ccde39cea78803
Brice Michallski <brice_michalski@carrefour.com>
2021-06-22 15:02:14 +0200

    missing @RestController

--- src/main/java/com/ynov/master/mobile/game/medieval/warfare/controller/NotificationController.java
+++ src/main/java/com/ynov/master/mobile/game/medieval/warfare/controller/NotificationController.java
@@ -4,17 +4,17 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.ynov.master.mobile.game.medieval.warfare.model.User;
 import com.ynov.master.mobile.game.medieval.warfare.service.NotificationService;
 import com.ynov.master.mobile.game.medieval.warfare.service.UserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+@RestController
 @RequestMapping("/notifications")
+@Api(tags = "users")
 public class NotificationController {
 
     @Autowired
